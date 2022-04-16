ba013d4c2b0f790e790b4144cbfab06b3ea50872
Tish254 <tiegomseeraoscar295@gmail.com>
2022-04-16 17:28:21 +0300

    first javascript commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+
+function hamburgerClicked() {
+
+    /* Called to Open the Mobile Menu */
+
+    console.log('Hamburgered');
+
+    document.querySelector('.fa-bars').classList.toggle('fa-xmark');
+
+    document.querySelector('.top').classList.toggle("nav__toggled");
+
+}
+
+function showAnswer() {
+    /* Called to to show the frequently asked questions answers */
+    
+    document.querySelectorAll('.faq').forEach( faq => {
+        faq.addEventListener('click', () => {
+
+            console.log('adding')
+            
+            let icon = faq.querySelector('i')
+            
+            if (icon.className === 'fa-solid fa-plus') {
+
+                icon.className = 'fa-solid fa-minus'
+                console.log('fa-minus added')
+                
+                
+            } else {
+                icon.className = 'fa-solid fa-plus'
+                console.log('fa-plus added')
+            }
+
+            faq.classList.toggle('faq__answer');
+            
+        })
+
+    })
+
+
+}
+
+showAnswer();
+
